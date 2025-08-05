b3ad647510ced3368eec07c25a6196fea72ca0bc
brandonherrera17143 <brandonhguerra17@gmail.com>
2025-08-04 21:59:25 -0600

    Se agrega arquetipo

---
+++ src/main/java/gt/ventas/reporte/VentasApplication.java
@@ -0,0 +1,13 @@
+package gt.ventas.reporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasApplication.class, args);
+	}
+
+}
